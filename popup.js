8e684c1337d73346fc77b6043d03497aa2c97cd9
ThaBlazee <38342167+ThaBlazee@users.noreply.github.com>
2023-11-26 12:44:24 -0800

    Webpage check error

--- popup.js
+++ popup.js
@@ -1,39 +1,41 @@
 document.addEventListener('DOMContentLoaded', function() {
   const profilesContainer = document.getElementById('profiles');
 
-  chrome.storage.local.get(['videoProfiles'], function(result) {
-    if (result.videoProfiles && result.videoProfiles.length > 0) {
-      // Sort the video profiles by quality
-      const sortedProfiles = result.videoProfiles.sort((a, b) => {
-        const qualityA = parseInt(a.quality.replace('p', ''), 10);
-        const qualityB = parseInt(b.quality.replace('p', ''), 10);
-        return qualityA - qualityB;
-      });
+  // Check if the current tab's URL is on the dropout.tv website
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    if (!tabs[0].url.includes('dropout.tv')) {
+      profilesContainer.textContent = 'Error: Not on dropout.tv website';
+      return;
+    }
 
-      sortedProfiles.forEach(profile => {
-        const downloadButton = document.createElement('button');
-        downloadButton.textContent = `${profile.quality}`;
-        downloadButton.addEventListener('click', function() {
-          chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+    chrome.storage.local.get(['videoProfiles'], function(result) {
+      if (result.videoProfiles && result.videoProfiles.length > 0) {
+        // Sort the video profiles by quality
+        const sortedProfiles = result.videoProfiles.sort((a, b) => {
+          const qualityA = parseInt(a.quality.replace('p', ''), 10);
+          const qualityB = parseInt(b.quality.replace('p', ''), 10);
+          return qualityA - qualityB;
+        });
+
+        sortedProfiles.forEach(profile => {
+          const downloadButton = document.createElement('button');
+          downloadButton.textContent = `${profile.quality}`;
+          downloadButton.addEventListener('click', function() {
             chrome.tabs.sendMessage(tabs[0].id, {action: "getTitle"}, response => {
-              let filename;
-              if (response && response.title) {
-                let validFilename = response.title.replace(/[\/:*?"<>|]/g, '_');
-                filename = `${validFilename}_${profile.quality}.mp4`;
-              } else {
-                filename = `video_${profile.quality}.mp4`;
-              }
+              let filename = response && response.title ? 
+                `${response.title.replace(/[\/:*?"<>|]/g, '_')}_${profile.quality}.mp4` : 
+                `video_${profile.quality}.mp4`;
               chrome.downloads.download({
                 url: profile.url,
                 filename: filename
               });
             });
           });
+          profilesContainer.appendChild(downloadButton);
         });
-        profilesContainer.appendChild(downloadButton);
-      });
-    } else {
-      profilesContainer.textContent = 'No video profiles found.';
-    }
+      } else {
+        profilesContainer.textContent = 'No video profiles found.';
+      }
+    });
   });
 });
